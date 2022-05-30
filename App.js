b0fc609879f971a076e1ca256c9d4401f81eef8b
Olek <48182795+Sniezka1927@users.noreply.github.com>
2022-05-30 14:42:53 +0200

    added Port

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.port;
 const router = require("./src/router");
 require("dotenv/config");
 const fs = require("fs");
